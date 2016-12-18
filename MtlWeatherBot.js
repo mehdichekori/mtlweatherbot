460b840d0ed83b7ccac8457739ac58ecde799d19
Mehdi Chekori <Mckworld@Macs-MacBook-Pro.local>
2016-12-17 23:30:19 -0500

    added comments

--- MtlWeatherBot.js
+++ MtlWeatherBot.js
@@ -9,22 +9,26 @@ var tempMin;
 var tempMax;
 var pressure;
 var weatherDescription;
-var windspeed;
+var windSpeed;
 
+//makes an an api call to the openweathermap api to get data about montreals weather
 GetData = function(){
   request({
     url:url,
     json: true,
   }, function (error,response,body){
+    //if there is no error and the response.statuscode is 200 (success)
     if(!error && response.statusCode === 200){
+      //sets the variables equal to the fetched data
       currentTemp = body.main.temp;
       tempMin = body.main.temp_min;
       tempMax = body.main.temp_max;
       humidity = body.main.humidity;
       weatherDescription = body.weather[0].description;
-      windspeed = body.wind.speed;
+      windSpeed = body.wind.speed;
 
-      console.log("Currently " + weatherDescription + " at " + currentTemp + "°C" + ", forcast: Min " + tempMin + "°C " + "& a Max of " + tempMax + "°C with " + humidity + "% Humidity" + " and "+windspeed+"m/s Wind");
+      console.log("Currently " + weatherDescription + " at " + currentTemp + "°C" + ", forcast: Min " + tempMin + "°C " + "& a Max of " + tempMax + "°C with " + humidity + "% Humidity" + " and "+windSpeed+"m/s Wind");
+
       useData();
     }
     if(error){
@@ -34,14 +38,34 @@ GetData = function(){
   })
 }
 
-function TweetTemp(){
-  //time = convertToServerTimeZone();
-
-  var tweet = {
-    status: "Currently " + weatherDescription + " at " + currentTemp + "°C" + ", forcast: Min " + tempMin + "°C " + "& a Max of " + tempMax + "°C with " + humidity + "% Humidity and " + windspeed +"km/h winds. #montreal #montrealweather"
+//uses the data that we fetched in getData()
+//then itconverts the windpseed to km/h
+//then it calls the function to tweet the data
+//if the current temp is different from the last one tweeted
+function useData(){
+  if(currentTemp != lastTemp){
+    windSpeed = convertWindSpeed(windSpeed);
+      TweetTemp();
+      console.log("The temperature has changed, tweeted the new value:");
+  }else{
+    console.log("The temperature hasn't changed since the last tweet waiting before sending another tweet.");
   }
+}
 
-  T.post('statuses/update',tweet,tweeted); //add check if currenttemp has load
+//converts windspeed from m/s to km/h
+function convertWindSpeed(speedMS){
+  var speedkmh = ((speedMS * 20) / 5);
+  return speedkmh;
+}
+
+//Uses Twitt object to post a tweet
+function TweetTemp(){
+  //construct the tweet
+  var tweet = {
+    status: "Currently " + weatherDescription + " at " + currentTemp + "°C" + ", forcast: Min " + tempMin + "°C " + "& a Max of " + tempMax + "°C with " + humidity + "% Humidity and " + windSpeed +"km/h winds. #montreal #montrealweather"
+  }
+  //posting the tweet, with a callback function tweeted
+  T.post('statuses/update',tweet,tweeted);
 
   function tweeted(err,data,response){
     if(err){
@@ -54,35 +78,5 @@ function TweetTemp(){
   }
 }
 
-
-// function convertToServerTimeZone(){
-//     //EST
-//     offset = -5.0
-//     clientDate = new Date();
-//     utc = clientDate.getTime() + (clientDate.getTimezoneOffset() * 60000);
-//     serverDate = new Date(utc + (3600000*offset));
-//     var time = serverDate+"";
-//     console.log(serverDate.toLocaleString());
-//     return serverDate.toLocaleString()
-// }
-
-//converts windspeed from m/s to km/h
-function convertWindSpeed(speedMS){
-  var speedkmh = ((speedMS * 20) / 5);
-  return speedkmh;
-}
-//uses teh data that we fetched,
-//converts the windpseed to km/h
-//and calls the function to tweet the data
-//if the current temp is different from the last one tweeted
-function useData(){
-  if(currentTemp != lastTemp){
-    windspeed = convertWindSpeed(windspeed);
-      TweetTemp();
-      console.log("The temperature has changed, tweeted the new value:");
-  }else{
-    console.log("The temperature hasn't changed since the last tweet waiting before sending another tweet.");
-  }
-}
 GetData();
 setInterval(GetData,1000*60); //calls the getData function once every minute
