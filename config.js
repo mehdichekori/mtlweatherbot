a307927e3148aef3c72283bd7bfd84df5ba17613
mehdichekori <mehdichekori@gmail.com>
2016-12-18 13:14:46 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'hbWlJAVWfJISgncs38m5AtBtp', 
+  consumer_secret:      'im0NqprRoM1jnzSnUMJvdZDW0S5WINSUeJHMAD6DW8T90Ai3yG',
+  access_token:         '809949568586620928-6NHYtpsNgfUZ3UMoCHGHR4nIWdsyUHf',
+  access_token_secret:  'wtaeYIx8wf8qCEcWdBkW0CzvObk1PNXCrjktzpQ7Xb2O0'
+}
